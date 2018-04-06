e7db5e0c46eb6ae9801861baa51dcc7cea1ca288
zerobim7 <deithk@gmail.com>
2018-04-06 13:22:59 +0900

    reactjs mofify

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,19 +3,61 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movies = ["movie1", "movie2", "movie3"];
-const movieImages = ["https://www.rodalesorganiclife.com/sites/rodalesorganiclife.com/files/styles/article_main_custom_user_desktop_1x/public/articles/2017/08/dogwithplant-1000.jpg?itok=s3hiogVF&timestamp=1502817275",
-"https://www.pets4homes.co.uk/images/articles/1144/large/buying-a-puppy-the-most-important-questions-to-ask-the-breeder-55ae636635ead.jpg",
-"https://www.telegraph.co.uk/content/dam/Pets/spark/pets-at-home-2017/fluffy-white-puppy-xlarge.jpg"];
+
 
 class App extends Component {
+
+componentWillMount(){
+  console.log('willmount');
+}
+
+componentDidMount(){
+  setTimeout(() => {
+    this.setState({
+      movies : [
+      {
+        title : "movie1",
+        poster : "https://www.rodalesorganiclife.com/sites/rodalesorganiclife.com/files/styles/article_main_custom_user_desktop_1x/public/articles/2017/08/dogwithplant-1000.jpg?itok=s3hiogVF&timestamp=1502817275"
+      },
+      {
+        title : "movie2",
+        poster : "https://www.pets4homes.co.uk/images/articles/1144/large/buying-a-puppy-the-most-important-questions-to-ask-the-breeder-55ae636635ead.jpg"
+      },
+      {
+        title : "movie3",
+        poster : "https://www.telegraph.co.uk/content/dam/Pets/spark/pets-at-home-2017/fluffy-white-puppy-xlarge.jpg"
+      },
+      {
+        title : "movie4",
+        poster : "https://www.pets4homes.co.uk/images/articles/1144/large/buying-a-puppy-the-most-important-questions-to-ask-the-breeder-55ae636635ead.jpg"
+      }
+      ]
+    })
+  }, 5000)
+}
+
+state = {
+
+}
+
+_renderMovies = () => {
+  const movies = this.state.movies.map((movie, index) => {
+      return <Movie title = {movie.title} poster = {movie.poster} key = {index}/>
+  });
+  return movies
+}
+
   render() {
+    console.log('render');
     return (
       <div className="App">
 
-        <Movie title={movies[0]} poster={movieImages[0]}/>
-        <Movie title={movies[1]} poster={movieImages[1]}/>
-        <Movie title={movies[2]} poster={movieImages[2]}/>
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+
+
+      {/*  {this.state.movies.map((movie, index) => {
+          return <Movie title = {movie.title} poster = {movie.poster} key = {index}/>
+        })};  */}
 
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import './Movie.css';
+import PropTypes from 'prop-types';
 
 class Movie extends Component{
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+  }
+
   render(){
     console.log(this.props);
     return(
@@ -14,7 +21,13 @@ class Movie extends Component{
   }
 }
 
+
 class MoviePoster extends Component {
+
+  static propTypes = {
+      poster2: PropTypes.string.isRequired
+  }
+
   render(){
     return(
       <img src= {this.props.poster2}/>
